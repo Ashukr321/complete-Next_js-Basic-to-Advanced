720ce52be6442fd402a98d1155c4cf9e1fefb342
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-11-06 17:26:55 +0530

    routes handler

---
+++ routehandlerdemo/src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+  <div className="flex justify-center h-screen items-center">
+   <h1 className="text-3xl">welcome to learn about the routes handler</h1>
+  </div>
+  );
+}
