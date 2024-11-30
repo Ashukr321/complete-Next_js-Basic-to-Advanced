e75fe44982363a9ef06fa35f343480506b99885c
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-11-30 22:12:58 +0530

    add portfolio webisite

---
+++ profile/src/components/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div id='about' className='h-screen flex justify-center items-center bg-black '>
+      <h1 className='text-white'> about us page</h1>
+    </div>
+  )
+}
+
+export default About
---
+++ profile/src/components/Project.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Project = () => {
+  return (
+    <div id='project' className='h-screen flex justify-center items-center bg-black '>
+    <h1 className='text-white'>  project </h1>
+
+    </div>
+  )
+}
+
+export default Project
