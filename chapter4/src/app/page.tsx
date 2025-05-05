57c3e87f2be054dc982acbe44801a673db8e3120
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-05-05 18:33:57 +0530

    how link work in the next js

--- chapter4/src/app/page.tsx
+++ chapter4/src/app/page.tsx
@@ -1,8 +1,18 @@
 
+import Link from "next/link";
 export default function Home() {
   return (
   <h1 className="text-center p-4">
     api routes
+    <ul >
+      <Link href="/dashboard" className="mr-2"> 
+       go to dashbaord
+      </Link>
+
+      <Link href="/product">
+        product
+      </Link>
+    </ul>
   </h1>
   );
 }
