68583140decd80a4b0345af998ff9b50c321ffe8
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-05-08 19:28:41 +0530

    query paramters in the next js

--- routehandler/src/app/api/hello/route.ts
+++ routehandler/src/app/api/hello/route.ts
@@ -1,11 +1,31 @@
 import { NextRequest } from "next/server";
+
+// get filter user import { NextRequest } from 'next/server';
+
 const users = [
-  { id: "1", name: "Ashutosh", age: 21 },
-  { id: "2", name: "Amit", age: 22 },
+  { id: "1", name: "Alice", age: 25 },
+  { id: "2", name: "Bob", age: 30 },
+  { id: "3", name: "Charlie", age: 28 },
 ];
 
-export function GET() {
-  return Response.json({ users });
+export async function GET(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams;
+  const query = searchParams.get("query");
+  if (!query) {
+    return Response.json(users, {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  const filterUser = users.filter(user =>
+    user.name.toLowerCase().includes(query.toLowerCase())
+  );
+
+  return Response.json(filterUser, {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
 }
 
 export async function POST(req: NextRequest) {
