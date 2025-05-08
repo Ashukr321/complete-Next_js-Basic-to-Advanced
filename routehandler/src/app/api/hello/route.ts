42208ba4998209da10aa48427a3f401b94e3a86f
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-05-08 19:08:32 +0530

    add post route handler

--- routehandler/src/app/api/hello/route.ts
+++ routehandler/src/app/api/hello/route.ts
@@ -1,4 +1,4 @@
-
+import { NextRequest } from "next/server";
 const users = [
   { id: "1", name: "Ashutosh", age: 21 },
   { id: "2", name: "Amit", age: 22 },
@@ -8,3 +8,12 @@ export function GET() {
   return Response.json({ users });
 }
 
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+
+  users.push(body);
+  return Response.json(users, {
+    status: 201,
+    headers: { "Content-Type": "application/json" },
+  });
+}
