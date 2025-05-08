991936cf8d8b1a0bd3b142728dbd0bfa8b22f428
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-05-08 18:49:24 +0530

    patch route handler in the next js

--- routehandler/src/app/api/hello/[id]/route.ts
+++ routehandler/src/app/api/hello/[id]/route.ts
@@ -5,8 +5,34 @@ const users = [
   { id: "2", name: "Amit", age: 22 },
 ];
 
-export function GET(req: NextRequest, { params }: { params: { id: string } }) {
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
   const id = params.id;
   const user = users.find(user => user.id === id);
   return Response.json({ user });
+}
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const id = params.id;
+  const body = await req.json(); // get the body data
+  console.log(body);
+
+  // find the user index
+  const userIndex = users.findIndex(user => user.id === id);
+  if (userIndex === -1) {
+    return new Response(JSON.stringify({ error: "user not found 404!" }));
+  }
+
+  // update user
+  users[userIndex] = { ...users[userIndex], ...body };
+
+  return new Response(JSON.stringify({ user: users[userIndex] }), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
 }
--- routehandler/src/app/api/hello/route.ts
+++ routehandler/src/app/api/hello/route.ts
@@ -1,17 +1,10 @@
-import { NextRequest, NextResponse } from "next/server";
 
 const users = [
-  { id:"1", name: "Ashutosh", age: 21 },
-  { id:"2" ,name: "Amit", age: 22 },
+  { id: "1", name: "Ashutosh", age: 21 },
+  { id: "2", name: "Amit", age: 22 },
 ];
 
-// export function GET(request: NextRequest) {
-//   return NextResponse.json(users);
-// }
-
-
 export function GET() {
-  return  Response.json({users});
+  return Response.json({ users });
 }
- 
 
